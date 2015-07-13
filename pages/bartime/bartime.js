6aa325f79b0aafc1b44c043fe74478050b33966f
Bijianzhao <bjz91@qq.com>
2015-07-14 00:51:19 +0800

    bartime增加多年不同物种个数

--- pages/bartime/bartime.js
+++ pages/bartime/bartime.js
@@ -10,7 +10,20 @@ $.getJSON('bartime.json', function(data) {
 	// 使用
 	require(['echarts', 'echarts/chart/line', 'echarts/chart/bar'], function(ec) {
 
-		// 基于准备好的dom，初始化echarts图表
+		/*---------- 准备第一个Series ----------*/
+		var oriSeries = [];
+		for (var i = 0; i < data.bartime.body.data[0].name.length; i++) {
+			var obj = {
+				'name' : data.bartime.body.data[0].name[i],
+				'type' : 'bar',
+				'data' : data.bartime.body.data[0].value[i],
+				'barCategoryGap' : '50%' //柱间间隔
+			};
+
+			oriSeries.push(obj);
+		}
+
+		/*---------- 基于准备好的dom，初始化echarts图表 ----------*/
 		var myChart = ec.init(document.getElementById('container'), 'macarons');
 
 		var option = {
@@ -28,7 +41,8 @@ $.getJSON('bartime.json', function(data) {
 					}
 				},
 				autoPlay : true,
-				playInterval : 1000
+				playInterval : 1000,
+				notMerge : true
 			},
 			options : [{
 				title : {
@@ -40,7 +54,7 @@ $.getJSON('bartime.json', function(data) {
 				},
 				legend : {
 					x : 'right',
-					'data' : data.bartime.body.name,
+					'data' : data.bartime.body.data[0].name,
 				},
 				toolbox : {
 					'show' : true,
@@ -84,36 +98,81 @@ $.getJSON('bartime.json', function(data) {
 					'name' : '单位'
 				},
 				//第一个数据
-				series : [{
-					'name' : data.bartime.body.name[0],
-					'type' : 'bar',
-					'data' : data.bartime.body.data[0].value[0],
-					 barCategoryGap: "50%" //柱间间隔
-				}, {
-					'name' : data.bartime.body.name[1],
-					'type' : 'bar',
-					'data' : data.bartime.body.data[0].value[1],
-					 barCategoryGap: "50%" //柱间间隔
-				}]
+				series : oriSeries
 			}]
 		};
 
-		//推送后续数据
+		/*---------- 推送后续数据 ----------*/
 		for (var i = 1; i < data.bartime.body.data.length; i++) {
+			var newSeries = [];
+			for (var j = 0; j < data.bartime.body.data[i].name.length; j++) {
+				var obj = {
+					'name' : data.bartime.body.data[i].name[j],
+					'type' : 'bar',
+					'data' : data.bartime.body.data[i].value[j],
+					'barCategoryGap' : '50%' //柱间间隔
+				};
+				newSeries.push(obj);
+			}
 			var obj = {
 				title : {
-					'text' : data.bartime.body.data[i].text
+					'text' : data.bartime.body.data[i].text,
+					'subtext' : '数据来源：毕鉴昭'
 				},
-				series : [{
-					'data' : data.bartime.body.data[i].value[0]
-				}, {
-					'data' : data.bartime.body.data[i].value[1]
-				}]
+				tooltip : {
+					'trigger' : 'axis'
+				},
+				legend : {
+					x : 'right',
+					'data' : data.bartime.body.data[i].name,
+				},
+				toolbox : {
+					'show' : true,
+					orient : 'vertical',
+					x : 'right',
+					y : 'center',
+					'feature' : {
+						'mark' : {
+							'show' : true
+						},
+						'dataView' : {
+							'show' : true,
+							'readOnly' : false
+						},
+						'magicType' : {
+							'show' : true,
+							'type' : ['line', 'bar', 'stack', 'tiled']
+						},
+						'restore' : {
+							'show' : true
+						},
+						'saveAsImage' : {
+							'show' : true
+						}
+					}
+				},
+				calculable : true,
+				grid : {
+					'y' : 80,
+					'y2' : 100
+				},
+				xAxis : [{
+					'type' : 'category',
+					'axisLabel' : {
+						'interval' : 0
+					},
+					'data' : data.bartime.xname
+				}],
+				yAxis : {
+					'type' : 'value',
+					'name' : '单位'
+				},
+				series : newSeries
 			}
 			option.options.push(obj);
 		}
 
-		// 为echarts对象加载数据
+		/*---------- 为echarts对象加载数据 ----------*/
 		myChart.setOption(option);
 	});
 });

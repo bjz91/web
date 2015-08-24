9b973e05a3c1cd8ee52d1d94f2f6ce95d4c5d5f0
Bijianzhao <bjz91@qq.com>
2015-08-25 01:20:04 +0800

    v1.4 barsort添加config

--- pages/barsort/barsort.js
+++ pages/barsort/barsort.js
@@ -1,94 +1,98 @@
-$.getJSON('barsort.json', function(barsortdata) {
+function barsort(json_name) {
 
-	/*------------ 排序 ------------*/
-	var oriData = barsortdata.barsort.data;
-	var oriName = barsortdata.barsort.name;
-	var temp;
+	$.getJSON(json_name, function(barsortdata) {
 
-	for (var i = 0; i < oriData.length - 1; i++) {
-		var max = i;
-		//查找最小值
-		for (var j = i + 1; j < oriData.length; j++) {
-			if (oriData[max] < oriData[j]) {
-				max = j;
+		/*------------ 排序 ------------*/
+		var oriData = barsortdata.barsort.data;
+		var oriName = barsortdata.barsort.name;
+		var temp;
+
+		for (var i = 0; i < oriData.length - 1; i++) {
+			var max = i;
+			//查找最小值
+			for (var j = i + 1; j < oriData.length; j++) {
+				if (oriData[max] < oriData[j]) {
+					max = j;
+				}
+			}
+			if (max != i) {
+				temp = oriData[i];
+				oriData[i] = oriData[max];
+				oriData[max] = temp;
+
+				temp = oriName[i];
+				oriName[i] = oriName[max];
+				oriName[max] = temp;
 			}
 		}
-		if (max != i) {
-			temp = oriData[i];
-			oriData[i] = oriData[max];
-			oriData[max] = temp;
 
-			temp = oriName[i];
-			oriName[i] = oriName[max];
-			oriName[max] = temp;
-		}
-	}
+		/*------------ 画图 ------------*/
 
-	/*------------ 画图 ------------*/
+		// 路径配置
+		require.config({
+			paths : {
+				echarts : '../../src/echarts-2.2.4/build/dist'
+			}
+		});
 
-	// 路径配置
-	require.config({
-		paths : {
-			echarts : '../../src/echarts-2.2.4/build/dist'
-		}
-	});
+		// 使用
+		require(['echarts', 'echarts/chart/bar', 'echarts/chart/line' // 使用柱状图就加载bar模块，按需加载
+		], function(ec) {
 
-	// 使用
-	require(['echarts', 'echarts/chart/bar', 'echarts/chart/line' // 使用柱状图就加载bar模块，按需加载
-	], function(ec) {
+			// 基于准备好的dom，初始化echarts图表
+			var myChart = ec.init(document.getElementById('container'), 'macarons');
 
-		// 基于准备好的dom，初始化echarts图表
-		var myChart = ec.init(document.getElementById('container'), 'macarons');
-
-		var option = {
-			title : {
-				text : '多物种数值排序柱状图',
-				subtext : '数据来源：毕鉴昭'
-			},
-			tooltip : {
-				trigger : 'axis'
-			},
-			legend : {
-				data : ['单位']
-			},
-			toolbox : {
-				show : true,
-				feature : {
-					mark : {
-						show : true
-					},
-					dataView : {
-						show : true,
-						readOnly : false
-					},
-					magicType : {
-						show : true,
-						type : ['line', 'bar']
-					},
-					restore : {
-						show : true
-					},
-					saveAsImage : {
-						show : true
+			var option = {
+				title : {
+					text : '多物种数值排序柱状图',
+					subtext : '数据来源：毕鉴昭'
+				},
+				tooltip : {
+					trigger : 'axis'
+				},
+				legend : {
+					data : ['单位']
+				},
+				toolbox : {
+					show : true,
+					feature : {
+						mark : {
+							show : true
+						},
+						dataView : {
+							show : true,
+							readOnly : false
+						},
+						magicType : {
+							show : true,
+							type : ['line', 'bar']
+						},
+						restore : {
+							show : true
+						},
+						saveAsImage : {
+							show : true
+						}
 					}
-				}
-			},
-			calculable : true,
-			xAxis : [{
-				type : 'category',
-				data : oriName
-			}],
-			yAxis : [{
-				type : 'value'
-			}],
-			series : [{
-				name : '单位',
-				type : 'bar',
-				data : oriData
-			}]
-		};
+				},
+				calculable : true,
+				xAxis : [{
+					type : 'category',
+					data : oriName
+				}],
+				yAxis : [{
+					type : 'value'
+				}],
+				series : [{
+					name : '单位',
+					type : 'bar',
+					data : oriData
+				}]
+			};
 
-		// 为echarts对象加载数据
-		myChart.setOption(option);
+			// 为echarts对象加载数据
+			myChart.setOption(option);
+		});
 	});
-});
+
+}
---
+++ pages/barsort/barsortconfig.js
@@ -0,0 +1,1 @@
+barsort('barsort.json');
